6dcfa5a5755571ad0de4e50dad0a273dcadbffdc
Francesco De Giorgio <thecoolbreezers@gmail.com>
2024-06-26 14:57:37 +0200

    .

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,9 +23,13 @@ const Header = () => {
           <li><Link to="/">Home</Link></li>
           <li><Link to="/details">{t('details')}</Link></li>
           <li><Link to="/schedule">{t('schedule')}</Link></li>
-          <li><Link to="/travel">{t('travel_info')}</Link></li>
-          {(userRole === 'admin' || userRole === 'guest_italy') && <li><Link to="/accommodation">{t('accommodation')}</Link></li>}
-          {(userRole === 'admin' || userRole === 'guest_hungary') && <li><Link to="/accommodation-hungary">{t('accommodation')}</Link></li>}
+          {(userRole === 'admin'
+            || userRole === 'guest_italy'
+            || userRole === 'guest_italy') && <li><Link to="/travel">{t('travel_info')}</Link></li>}
+          {(userRole === 'admin'
+            || userRole === 'guest_italy') && <li><Link to="/accommodation">{t('accommodation')}</Link></li>}
+          {(userRole === 'admin'
+            || userRole === 'guest_hungary') && <li><Link to="/accommodation-hungary">{t('accommodation')}</Link></li>}
           <li><Link to="/gifts">{t('gifts')}</Link></li>
           <li><Link to="/poi">{t('poi')}</Link></li>
           <li><Link to="/rsvp">{t('rsvp')}</Link></li>
