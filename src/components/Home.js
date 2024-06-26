a3e772187339aac220642773ef982166f30bf489
Francesco De Giorgio <thecoolbreezers@gmail.com>
2024-06-26 12:59:24 +0200

    noi

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import noi from '../assets/noi.jpeg';
 const Home = () => {
   return (
     <section>
       <h2>Benvenuti al Nostro Matrimonio</h2>
       <p>Siamo felicissimi di condividere con voi questo giorno speciale. Grazie per essere parte della nostra vita!</p>
-      <img src="your-photo-url.jpg" alt="Foto di noi due" />
+      <img src={noi} alt="Foto di noi due" />
     </section>
   );
 }
