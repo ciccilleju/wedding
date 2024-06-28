d2f03a69e80ad027ea595a569f0b274b175a3c55
Francesco De Giorgio <thecoolbreezers@gmail.com>
2024-06-28 12:00:39 +0200

    userlist fix

--- src/components/UsersManagement/UsersList.js
+++ src/components/UsersManagement/UsersList.js
@@ -2,40 +2,41 @@ import React from 'react';
 
 const UserList = ({ users, handleUpdateUser, handleDeleteUser }) => {
   return (
-    <div className="p-4">
-      <ul className="space-y-4">
+    <div className="container mx-auto px-4 py-8">
+      <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-1">
         {users.map(user => (
-          <li key={user._id} className="flex flex-col md:flex-row items-center justify-between p-4 border rounded-lg shadow-md bg-white">
-            <div className="flex-1 text-center md:text-left">
-              <p className="text-lg font-semibold">Username: {user.username}</p>
-              <p className="text-lg font-semibold">Nome: {user.firstName}</p>
-              <p className="text-lg font-semibold">Cognome: {user.lastName}</p>
-              <p className="text-gray-600">Language: {user.language}</p>
-              <p className="text-gray-600">Role: {user.role}</p>
-              {user.attendance && <>
-                <p className="text-gray-600">Attendance: {user.attendance ? 'Yes' : 'No'}</p>
-                <p className="text-gray-600">Adults: {user.guests}</p>
-                <p className="text-gray-600">Children: {user.guests_children}</p>
-                <p className="text-gray-600">Intollerances: {user.notes}</p>
-              </>}
+          <div key={user._id} className="w-full mx-auto bg-white rounded-xl overflow-hidden shadow-lg border rounded-lg   bg-white">
+            <div className="p-6">
+              <h3 className="font-bold text-xl mb-2">{user.username}</h3>
+              <p className="text-gray-600 mb-4">{user.firstName} {user.lastName}</p>
+              <p className="text-gray-600 mb-2">Language: {user.language}</p>
+              <p className="text-gray-600 mb-2">Role: {user.role}</p>
+              {user.attendance && (
+                <>
+                  <p className="text-gray-600 mb-2">Attendance: {user.attendance ? 'Yes' : 'No'}</p>
+                  <p className="text-gray-600 mb-2">Adults: {user.guests}</p>
+                  <p className="text-gray-600 mb-2">Children: {user.guests_children}</p>
+                  <p className="text-gray-600 mb-2">Intollerances: {user.notes}</p>
+                </>
+              )}
+              <div className="mt-4 flex justify-end">
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2 focus:outline-none focus:shadow-outline"
+                  onClick={() => handleUpdateUser(user)}
+                >
+                  Update
+                </button>
+                <button
+                  className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  onClick={() => handleDeleteUser(user)}
+                >
+                  Delete
+                </button>
+              </div>
             </div>
-            <div className="mt-4 md:mt-0 flex  flex-col">
-              <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                onClick={() => handleUpdateUser(user)}
-              >
-                Update
-              </button>
-              <button
-                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-                onClick={() => handleDeleteUser(user)}
-              >
-                Delete
-              </button>
-            </div>
-          </li>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
