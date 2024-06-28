f4c34d63ca70fcd0d90c3e30faa98a547f67f849
Francesco De Giorgio <thecoolbreezers@gmail.com>
2024-06-28 10:20:04 +0200

    componente text-image

--- src/components/POI/Tropea/Tropea.js
+++ src/components/POI/Tropea/Tropea.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import tropea from '../../../assets/POI/tropea/tropea.jpg';
 import styles from '../Poi.module.scss';
+import TextImage from '../../text-image/TextImage';
 
 const Tropea = () => {
 
@@ -10,11 +11,18 @@ const Tropea = () => {
     <div className={styles.poi}>
       <h2 className="text-3xl font-bold underline">Tropea</h2>
 
-      <p>
-        Tropea, cittadina nota soprattutto per le sue spiagge bellissime, per i fondali ideali per chi ama le esplorazioni subacquee e per la sua gastronomia. È nota in tutto il mondo, infatti la Cipolla di Tropea, unica per la sua dolcezza, una dolcezza favorita dal clima e dalla vicinanza del mare. Il mare fu, nella sua storia, anche fonte di pericoli: per difenderla, gli Arabi costruirono la Torre Lunga sulla parte più alta della roccia, i Bizantini le mura e gli Aragonesi la Torre Maestra.
-      </p>
+      <TextImage
+        title={'Tropea'}
+        address={'89861 VV'}
+        maxHeight={300}
+        children={<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12459.509726082722!2d15.873664578391413!3d38.67468483617541!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13157123bc33f243%3A0x23374535c06835f7!2s89861%20Tropea%20VV!5e0!3m2!1sit!2sit!4v1719560419599!5m2!1sit!2sit" width="400" height="180" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        }
+        text={'Tropea, cittadina nota soprattutto per le sue spiagge bellissime, per i fondali ideali per chi ama le esplorazioni subacquee e per la sua gastronomia. È nota in tutto il mondo, infatti la Cipolla di Tropea, unica per la sua dolcezza, una dolcezza favorita dal clima e dalla vicinanza del mare. Il mare fu, nella sua storia, anche fonte di pericoli: per difenderla, gli Arabi costruirono la Torre Lunga sulla parte più alta della roccia, i Bizantini le mura e gli Aragonesi la Torre Maestra.'} />
+
+
       <img src={tropea} alt="Tropea" />
-      <p>Da vivere, poi, è il centro storico, dove il profumo della salsedine pervade l’aria e i ristorantini offrono imperdibili piatti a base di pesce, da gustare magari all’aperto per poi andare a guardare il mare che, nel buio della notte, riflette le mille luci della città  </p>
+
+
 
 
 
---
+++ src/components/text-image/TextImage.js
@@ -0,0 +1,37 @@
+
+
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+const TextImage = ({ inverse, children, text, title, address, maxHeight }) => {
+
+  const { t } = useTranslation();
+  return (
+    <>
+      <div class={`${inverse ? 'lg:flex-row-reverse' : 'lg:flex-row'} flex flex-col lg:justify-between mb-4`}>
+        <p class={`${inverse ? 'lg:ml-3' : 'lg:mr-3'} w-full lg:basis-2/3 lg:flex-grow-0  mb-5 lg:mb-0`}>
+          {text}
+        </p>
+        <div style={{ maxHeight: maxHeight }} class="w-full lg:basis-1/3 lg:flex-grow-0 lg:sticky lg:top-0  rounded-xl  shadow-lg cursor-pointer p-2.5 py-2 border-2 border-solid ">
+          {children}
+          <div className="py-4">
+            <h2 className="font-bold text-xl mb-2">{title}</h2>
+            <p className="text-gray-700 text-base">
+              {address}
+            </p>
+          </div>
+        </div>
+
+      </div>
+
+    </>
+  );
+}
+
+export default TextImage;
+
+
+
+
+
+
